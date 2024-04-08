0964d159dbbb29c28bbd97eb75f20c8c84d1b246
Mehdiznayzen <mehdiznayzen@gmail.com>
2024-04-08 22:06:10 +0000

    Edit onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,20 +1,24 @@
-import AccountProfile from "@/components/forms/AccountProfile"
-import { currentUser } from '@clerk/nextjs'
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
+import { fetchUser } from "@/lib/actions/user.actions";
+import AccountProfile from "@/components/forms/AccountProfile";
 
-const Page = async () => {
-    const user = await currentUser()
+async function Page() {
+    const user = await currentUser();
+    if (!user) return null; // to avoid typescript warnings
 
-    const userInfo = {}
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
 
     const userData = {
-        id : user?.id,
-        objectId : userInfo?._id,
-        username: user?.username,
-        name: user?.firstName ,
+        id: user.id,
+        objectId: userInfo?._id,
+        username: userInfo ? userInfo?.username : user.username,
+        name: userInfo ? userInfo?.name : user.firstName ?? "",
         bio: userInfo ? userInfo?.bio : "",
-        image: user?.imageUrl,
-    }
+        image: userInfo ? userInfo?.image : user.imageUrl,
+    };
 
     return (
         <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
@@ -22,14 +26,15 @@ const Page = async () => {
             <p className='mt-3 text-base-regular text-light-2'>
                 Complete your profile now, to use Threds.
             </p>
-            <section className="mt-9 bg-dark-2 p-10 rounded-xl">
-                <AccountProfile
-                    user={userData}
-                    btnTitle = 'Continue'
+
+            <section className='mt-9 bg-dark-2 p-10'>
+                <AccountProfile 
+                    user={userData} 
+                    btnTitle='Continue' 
                 />
             </section>
         </main>
-    )
+    );
 }
 
-export default Page
+export default Page;
